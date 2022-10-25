e788b21cf787b68fd1742703f1ac0e2124dbc482
Jadon <57832214+Tabletop1000@users.noreply.github.com>
2022-10-25 20:50:54 +1100

    Initial commit

---
+++ build/CMakeFiles/ReportGen.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReportGen.dir/Main.cpp.obj"
+  "CMakeFiles/ReportGen.dir/Main.cpp.obj.d"
+  "ReportGen.exe"
+  "ReportGen.exe.manifest"
+  "ReportGen.pdb"
+  "libReportGen.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ReportGen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
